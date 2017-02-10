13efe39c2c5951451d42e24678e5d82d5bc4cf90
YSI\GabrielC <calindotgabriel18@gmail.com>
2017-02-10 14:13:42 +0200

    Notification feature

--- app/src/main/java/com/gabrielc/worklog/MainActivity.java
+++ app/src/main/java/com/gabrielc/worklog/MainActivity.java
@@ -1,12 +1,14 @@
 package com.gabrielc.worklog;
 
 import android.app.ActivityManager;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.orhanobut.logger.Logger;
@@ -14,9 +16,14 @@ import com.orhanobut.logger.Logger;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String MINUTES_TO_COUNTDOWN = "MINUTES_TO_COUNTDOWN";
+    public static final int NOTIFICATION_ID = 1;
+
+    private NotificationCompat.Builder mNotificationBuilder;
+    private NotificationManager mNotificationManager;
 
     @BindView(R.id.main_tv)
     TextView mMainTv;
@@ -28,12 +35,23 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Logger.d("onCreate");
 
+        mNotificationBuilder = new NotificationCompat.Builder(this)
+                .setContentText("X secs lefts")
+                .setContentTitle("WorkLog")
+                .setSmallIcon(R.drawable.pugnotification_ic_launcher)
+                .setAutoCancel(false)
+                .setOngoing(true)
+                .setOnlyAlertOnce(true);
+
         registerReceiver(mCountdownReceiver, new IntentFilter(CountdownService.COUNTDOWN_BR));
         if (!isMyServiceRunning(CountdownService.class)) {
+            Logger.d("Starting service..");
             final Intent serviceIntent = new Intent(MainActivity.this, CountdownService.class);
             serviceIntent.putExtra(MINUTES_TO_COUNTDOWN, 1L);
             startService(serviceIntent);
         }
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(NOTIFICATION_ID, mNotificationBuilder.build());
     }
 
     @Override
@@ -42,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
         Logger.d("onPause");
 
 //        stopService(new Intent(MainActivity.this, CountdownService.class));
-        unregisterReceiver(mCountdownReceiver);
+//        unregisterReceiver(mCountdownReceiver);
     }
 
     private BroadcastReceiver mCountdownReceiver = new BroadcastReceiver() {
@@ -51,7 +69,10 @@ public class MainActivity extends AppCompatActivity {
             if (intent.getExtras() != null) {
                 final long secsUntilFinished = intent.getLongExtra(CountdownService.KEY_COUNTDOWN_INTENT, 0);
                 Logger.d("Activity received %d", secsUntilFinished);
-                mMainTv.setText(secsUntilFinished + " secounds left");
+                final String text = secsUntilFinished + " secounds left";
+                mMainTv.setText(text);
+                mNotificationBuilder.setContentText(text);
+                mNotificationManager.notify(NOTIFICATION_ID, mNotificationBuilder.build());
             }
         }
     };
